caf10174b3ec18f4a3f1c6ef6fb509d160fc5ac2
Wang Yaoshen <wangyaoshen@zhonghegene.com>
2024-01-24 16:39:38 +0800

    ✨ support multi seq from fasta

--- util/ProteinaseK/enzymeDigestion.pl
+++ util/ProteinaseK/enzymeDigestion.pl
@@ -3,6 +3,7 @@
 use strict;
 use warnings;
 
+# // 脂肪氨基酸
 my%AliphaticAminoAcids=(
     'A'=>'Ala', # 丙氨酸
     'C'=>'Cys', # 半胱氨酸
@@ -24,6 +25,7 @@ my%AliphaticAminoAcids=(
     # 'H'=>'His', # 组氨酸
     # 'P'=>'Pro', # 脯氨酸
 
+# // 芳香族胺基酸
 my%AromaticAminoAcids=(
     'F'=>'Phe', # 苯丙氨酸
     'W'=>'Trp', # 色氨酸
@@ -32,19 +34,69 @@ my%AromaticAminoAcids=(
 #!/usr/bin/perl
 
 # ANSI 转义码
-my $color_red = "\e[40m\e[31m";      # 红色前景色
-my $color_green = "\e[40m\e[32m";    # 绿色前景色
-my $color_yellow = "\e[40m\e[33m";    # 绿色前景色
-my $color_blue = "\e[40m\e[34m";    # 绿色前景色
-my $color_reset = "\e[0m\e[0m";     # 重置颜色
+# my $color_bg ="\e[47m";
+my $color_bg ="\e[1m\e[40m";
+my $color_red = "$color_bg\e[91m";      # 红色前景色
+my $color_green = "$color_bg\e[92m";    # 绿色前景色
+my $color_yellow = "$color_bg\e[93m";    # 绿色前景色
+my $color_blue = "$color_bg\e[94m";    # 绿色前景色
+my $color_white = "$color_bg\e[97m";      # 红色前景色
+my $color_reset = "\e[0m";     # 重置颜色
 
 
 my$tag=0;
-my$break=80;
 my$count=0;
+my$break=100;
 while(<>){
+    if(/>/){
+        print "\n$_";
+        $tag=0;
+        $count=0;
+        next;
+    }
     chomp;
     my@c=split //,$_;
+    for(@c){
+        $count++;
+        if(exists $AliphaticAminoAcids{$_}){
+            if($tag+1==0){
+                print "${color_red}|${color_reset}";
+                if ($count%$break==0){
+                   print "\n";
+                }
+                $count++;
+            }
+            $tag=1;
+            print "${color_yellow}$_${color_reset}";
+        }elsif(exists $AromaticAminoAcids{$_}){
+            if($tag-1==0){
+                print "${color_red}|${color_reset}";
+                if ($count%$break==0){
+                  print "\n";
+                }
+                $count++;
+            }
+            $tag=-1;
+            print "${color_green}$_${color_reset}";
+        }else{
+            $tag=0;
+            print "${color_white}$_${color_reset}";
+        }
+        if ($count%$break==0){
+            print "\n";
+        }
+    }
+    # print "\n";
+    # $count=0;
+}
+print "\n";
+
+
+sub digestion(){
+    my$count=0;
+    my$seq=shift;
+
+    my@c=split //,$seq;
     for(@c){
         $count++;
         if(exists $AliphaticAminoAcids{$_}){
@@ -76,7 +128,6 @@ while(<>){
         }
     }
     print "\n";
-    $count=0;
 }
 
 
